81dccbf86eee1db5bbd00de96b269ba2e7415562
Xoaquin Castrelo <xoaquin.cb@gmail.com>
2022-05-01 14:01:33 +0100

    Fixed errors in the PID loop.

--- src/pid.c
+++ src/pid.c
@@ -1,29 +1,21 @@
-void update_pid(struct motor_pid *mp, uint16_t new_pos)
+#include "pid.h"
+
+void update_pid(struct motor_pid *mp, int16_t new_pos)
 {
     int16_t error = new_pos - mp->position;
 
-    if (mp->integral > -integral_limit && mp->integral < integral_limit)
+    if (mp->integral > -mp->integral_limit && mp->integral < mp->integral_limit)
         mp->integral += error;
 
     int16_t derivative = error - mp->last_error;
-
-    int16_t correct = kp * error + ki * integral + kd * derivative;
-
-    int16_t tpwm = mp->pwm + correct;
-    if (tpwm < lower_limit)
-        mp->pwm = lower_limit;
-    else if (tpwm > upper_limit)
-        mp->pwm = upper_limit;
+    mp->last_error = error;
+    int16_t correct = mp->kp * error + mp->ki * mp->integral + mp->kd * derivative;
+    uint16_t tpwm = mp->pwm - correct;
+    
+    if (tpwm < mp->lower_limit)
+        mp->pwm = mp->lower_limit;
+    else if (tpwm > mp->upper_limit)
+        mp->pwm = mp->upper_limit;
     else
         mp->pwm = tpwm;
 }
-
-/*
-struct motor_pid mp1;
-while (1)
-{
-    uint16_t rot = read_rotary()
-        update_pid(&mp1, rot);
-    set_pwm(mp1.pwm);
-}
-*/
--- src/pid.h
+++ src/pid.h
@@ -4,13 +4,13 @@
 
 struct motor_pid
 {
-    uint16_t position;
+    int16_t position;
     int16_t last_error, integral;
     uint16_t kp, ki, kd;
     uint16_t pwm;
     uint16_t integral_limit, lower_limit, upper_limit;
 };
 
-void update_pid(struct motor_pid *mp, uint16_t new_pos);
+void update_pid(struct motor_pid *mp, int16_t new_pos);
 
-#endif // PID_H
+#endif /* PID_H */
